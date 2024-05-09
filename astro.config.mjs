ac2b5beb4ca7627ebf8e4a1f443b87a58ac38bf3
Alex Lee <hyunho98@gmail.com>
2024-05-09 16:26:47 -0400

    Add astro island component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blog-tutorial-alex.netlify.app/"
+  site: "https://blog-tutorial-alex.netlify.app/",
+  integrations: [preact()]
 });
