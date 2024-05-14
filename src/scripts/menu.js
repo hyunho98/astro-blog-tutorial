f94c7bb2034b825b82da773ddfc4e361931a8b39
Alex Lee <hyunho98@gmail.com>
2024-05-14 15:50:37 -0400

    Fix scripts now reload on page transition

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,2 +1,4 @@
-document.querySelector('.hamburger').addEventListener('click', () =>
-    document.querySelector('.nav-links').classList.toggle('expanded'))
+document.addEventListener('astro:page-load', () => {
+    document.querySelector('.hamburger').addEventListener('click', () =>
+        document.querySelector('.nav-links').classList.toggle('expanded'))
+})
